6f4c010e91b460bcc11e9dcb4b56ea46d521d37a
Maimuna Rahman <maimunarahman30@gmail.com>
2025-12-05 01:27:16 +0600

    enrollment

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,10 @@ app.use(cookieParser())
 app.use(express.json())
 app.use(
   cors({
-    origin: "http://localhost:8080",
+    origin:[ "http://localhost:8080",
+      "https://courseflow-platform.vercel.app"
+    ],
+
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   })
